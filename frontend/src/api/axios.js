94dac2b90c96b9cef25401e8b09f6e70af437740
Pravin <pravinpagar290@gmail.com>
2025-12-24 22:59:51 +0530

    Fix: update backend URL configuration to use environment variable

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const backend = "http://localhost:8000";
+const backend = process.env.REACT_APP_BACKEND_URL || "http://localhost:8000";
 const baseURL = backend.replace(/\/+$/, "") + "/api/v1";
 
 const api = axios.create({
